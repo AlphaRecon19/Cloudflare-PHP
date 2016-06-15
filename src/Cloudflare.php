7b9353022df7f6974cf9e90b15d313fa4d886a28
Chris Hilsdon <chris@koolserve.uk>
2016-06-15 10:44:51 +0100

    Allow Guzzle to be injected into the construct

--- src/Cloudflare.php
+++ src/Cloudflare.php
@@ -5,6 +5,7 @@
 namespace Cloudflare;
 
 use Symfony\Component\Yaml\Yaml;
+use GuzzleHttp\Client;
 
 class Cloudflare
 {
@@ -12,6 +13,12 @@ class Cloudflare
     protected $APIKey;
     protected $Email;
     protected $MakeRequests = true;
+    protected $Guzzle;
+
+    public function setGuzzle($Guzzle)
+    {
+        $this->Guzzle = $Guzzle;
+    }
 
     public function getEndpoint()
     {
@@ -33,6 +40,18 @@ class Cloudflare
         return $this->MakeRequests;
     }
 
+    public function getGuzzle()
+    {
+        //If Guzzle has not been setup yet
+        if($this->Guzzle === null) {
+            var_dump('Guzzle was created');
+            $Guzzle = new Client();
+            $this->setGuzzle($Guzzle);
+        }
+
+        return $this->Guzzle;
+    }
+
     public function diableRequests()
     {
         $this->MakeRequests = false;
@@ -43,7 +62,7 @@ class Cloudflare
         $this->MakeRequests = true;
     }
 
-    public function __construct($options = null)
+    public function __construct($options = null, $Guzzle = null)
     {
         if (is_null($options) || !is_array($options)) {
             $this->loadConfig();
@@ -51,6 +70,11 @@ class Cloudflare
             $this->APIKey = $options['APIKey'];
             $this->Email = $options['Email'];
         }
+
+        if (!is_null($Guzzle)) {
+            $this->setGuzzle($Guzzle);
+        }
+
     }
 
     private function loadConfig()
